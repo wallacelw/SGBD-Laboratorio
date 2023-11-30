a91086ab9af653510c8a13e0cd3bdc2a196af552
Dickmann25 <106775626+Dickmann25@users.noreply.github.com>
2023-11-30 10:02:19 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ import cors from 'cors'
 const app = express()
 app.use(cors());
 
+/*
+    Aqui ficarão as funções para a tabela dos livros.
+*/
+
 // GET Livros. Através de uma requisição GET, retorna todos os Livros da base de dados
 app.get("/livros", async (req, res) => {
     const livros = await db.getLivros();
@@ -21,7 +25,28 @@ app.get("/livros", async (req, res) => {
 // GET Livros com base no isbn. Através de uma requisição GET, retorna o Livro da base de dados com o isbn correspondente
 app.get("/livro/:isbn", async (req, res) => {
     const isbn = req.params.isbn
-    const livro = await db.getLivro(isbn);
+    const livro = await db.getBuscalivro("isbn", isbn);
+    res.status(200).send(livro)
+})
+
+// GET isbn com base no autor. Através de uma requisição GET, retorna o isbn da base de dados com o autor correspondente
+app.get("/autor/:autor", async (req, res) => {
+    const autor = req.params.autor
+    const livro = await db.getAutorlivro(autor);
+    res.status(200).send(livro)
+})
+
+// GET Livros com base no titulo. Através de uma requisição GET, retorna o Livro da base de dados com o titulo correspondente
+app.get("/livro/:titulo", async (req, res) => {
+    const titulo = req.params.titulo
+    const livro = await db.getBuscalivro("titulo", titulo);
+    res.status(200).send(livro)
+})
+
+// GET isbn com base no categoria. Através de uma requisição GET, retorna o isbn da base de dados com o categoria correspondente
+app.get("/categoria_dos_livros/:categoria", async (req, res) => {
+    const categoria = req.params.categoria
+    const livro = await db.getCategorialivro(categoria);
     res.status(200).send(livro)
 })
 
@@ -32,24 +57,64 @@ app.post("/livro", async (req, res) => {
     res.status(201).send(livro)
 })
 
-// GET Livros. Através de uma requisição GET, retorna todos os Livros da base de dados
+/*
+    Aqui ficarão as funções para a tabela dos materiais.
+*/
+
+// GET Materiais. Através de uma requisição GET, retorna todos os Materiais da base de dados
 app.get("/materiais", async (req, res) => {
     const materiais = await db.getMateriais();
     res.status(200).send(materiais)
 })
 
-// GET Livros com base no isbn. Através de uma requisição GET, retorna o Livro da base de dados com o isbn correspondente
+// GET Materiais com base no id. Através de uma requisição GET, retorna o Material da base de dados com o id correspondente
 app.get("/material/:id", async (req, res) => {
     const id = req.params.id
     const material = await db.getMaterial(id);
     res.status(200).send(material)
 })
 
-// POST Livro. Cria um Livro novo na base de dados de acordo com as informações passadas
+// GET Materiais com base no id. Através de uma requisição GET, retorna o Material da base de dados com o id correspondente
+app.get("/material/:descrição", async (req, res) => {
+    const descrição = req.params.descrição
+    const material = await db.getMaterial(descrição);
+    res.status(200).send(material)
+})
+
+app.get("/material/:numero_de_serie", async (req, res) => {
+    const numero_de_serie = req.params.numero_de_serie
+    const material = await db.getMaterial(numero_de_serie);
+    res.status(200).send(material)
+})
+
+app.get("/material/:estado_de_conservação", async (req, res) => {
+    const estado_de_conservação = req.params.estado_de_conservação
+    const material = await db.getMaterial(estado_de_conservação);
+    res.status(200).send(material)
+})
+
+// POST Materiais. Cria um Materiais novo na base de dados de acordo com as informações passadas
 app.post("/material", async (req, res) => {
     const {id, descricao, numero_de_serie, data_de_aquisicao, estado_de_conservacao, localizacao_fisica, uri_da_foto_do_material} = req.body
     const material = await db.createMaterial(id, descricao, numero_de_serie, data_de_aquisicao, estado_de_conservacao, localizacao_fisica, uri_da_foto_do_material)
     res.status(201).send(material)
+})
+
+/*
+    Aqui ficarão as funções para a tabela dos emprestimos.
+*/
+
+// GET Emprestimos. Através de uma requisição GET, retorna todos os Emprestimos da base de dados
+app.get("/emprestimos", async (req, res) => {
+    const emprestimos = await db.getEmprestimo();
+    res.status(200).send(emprestimos)
+})
+
+// POST Emprestimos. Cria um Emprestimos novo na base de dados de acordo com as informações passadas
+app.post("/livro", async (req, res) => {
+    const {id_do_livro, id_do_material, id_do_usuario, tipo_do_item, data_do_emprestimo, data_de_devolucao_prevista, status_do_emprestimo} = req.body
+    const emprestimos = await db.createLivro(id_do_livro, id_do_material, id_do_usuario, tipo_do_item, data_do_emprestimo, data_de_devolucao_prevista, status_do_emprestimo)
+    res.status(201).send(emprestimos)
 })
 
 // Abre o servidor local na porta 3333
--- database.js
+++ database.js
@@ -10,7 +10,7 @@ import mysql from "mysql2"
 const pool = mysql.createPool({
     host: '127.0.0.1',
     user: 'root',
-    password: '123456',
+    password: 'Jv28042001',
     database: 'projeto_db'
 }).promise()
 
@@ -21,11 +21,23 @@ export async function getLivros() {
     return result
 }
 
-export async function getLivro(isbn) {
-    const [result] = await pool.query("SELECT * FROM Livros WHERE isbn = ?", [isbn])
+// Função que faz a query para buscar os livros baseado no parametro
+export async function getBuscalivro(parametro,dados) {
+    const [result] = await pool.query(`SELECT * FROM Livros WHERE ${parametro} = "${dados}"`)
     return result
 }
 
+export async function getAutorlivro(autor) {
+    const [result] = await pool.query("SELECT isbn FROM Autor WHERE autor = ?", [autor])
+    return result
+}
+
+export async function getCategorialivro(categoria) {
+    const [result] = await pool.query("SELECT isbn FROM categoria_dos_livros WHERE categoria = ?", [categoria])
+    return result
+}
+
+// Função que cria livro
 export async function createLivro(isbn, titulo, descricao, data, estado, loc, uri) {
     const result = await pool.query(`
     INSERT INTO Livros (isbn,
@@ -40,20 +52,26 @@ export async function createLivro(isbn, titulo, descricao, data, estado, loc, ur
     return result
 }
 
-
+// Função que faz a query para buscar todos os materiais do DB
 export async function getMateriais() {
     const [result] = await pool.query("SELECT * FROM Materiais_Ditaticos")
     return result
 }
 
-export async function getMaterial(id) {
-    const [result] = await pool.query("SELECT * FROM Materiais_Ditaticos WHERE id = ?", [id])
+// Função que faz a query para buscar os materiais baseado no parametro
+export async function getBuscamateriais(parametro,dados) {
+    const [result] = await pool.query(`SELECT * FROM Materiais_Ditaticos WHERE ${parametro} = "${dados}"`)
+    return result
+}
+
+export async function getCategoriamaterial(categoria) {
+    const [result] = await pool.query("SELECT isbn FROM categoria_dos_materiais WHERE categoria = ?", [categoria])
     return result
 }
 
 export async function createMaterial(id, descricao, numero_de_serie, data_de_aquisicao, estado_de_conservacao, localizacao_fisica, uri_da_foto_do_material) {
     const result = await pool.query(`
-    INSERT INTO Livros (id,
+    INSERT INTO Materiais_Ditaticos (id,
                         descricao,
                         numero_de_serie,
                         data_de_aquisicao,
@@ -62,5 +80,32 @@ export async function createMaterial(id, descricao, numero_de_serie, data_de_aqu
                         uri_da_foto_do_material)
     VALUES (?, ?, ?, ?, ?, ?, ?)
     `, [id, descricao, numero_de_serie, data_de_aquisicao, estado_de_conservacao, localizacao_fisica, uri_da_foto_do_material])
+    return result
+}
+
+// Busca todos os emprestimos
+export async function getEmprestimo() {
+    const [result] = await pool.query("SELECT * FROM Emprestimos")
+    return result
+}
+
+// Cria emprestimos
+export async function createEmprestimo(id_do_livro, id_do_material, id_do_usuario, tipo_do_item, data_do_emprestimo, data_de_devolucao_prevista, status_do_emprestimo) {
+    const result = await pool.query(`
+    INSERT INTO Emprestimos (id_do_livro,
+                        id_do_material,
+                        id_do_usuario,
+                        tipo_do_item,
+                        data_do_emprestimo,
+                        data_de_devolucao_prevista,
+                        status_do_emprestimo)
+    VALUES (?, ?, ?, ?, ?, ?, ?)
+    `, [id_do_livro, id_do_material, id_do_usuario, tipo_do_item, data_do_emprestimo, data_de_devolucao_prevista, status_do_emprestimo])
+    return result
+}
+
+// atualiza data de emprestimo
+export async function atualizaEmprestimo(id_do_livro, date) {
+    const [result] = await pool.query(`UPDATE Emprestimos SET data_de_devolucao_prevista = "${date}" WHERE id_do_livro = "${id_do_livro}"` )
     return result
 }
