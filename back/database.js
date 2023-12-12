c009a0dc057b9c10178038a7930e963f56ea9851
wallacelw <wallacebtlw@gmail.com>
2023-12-12 00:18:18 -0300

    extra functions commented, name_bugfix

--- back/database.js
+++ back/database.js
@@ -33,49 +33,49 @@ export async function getLivros() {
     return result
 }
 
-// Função que faz a query para buscar os livros baseado no parametro
-export async function getBuscalivro(parametro, dados) {
-    const [result] = await pool.query(`SELECT * FROM Livros WHERE ${parametro} = "${dados}"`)
-    return result
-}
+// // Função que faz a query para buscar os livros baseado no parametro
+// export async function getBuscalivro(parametro, dados) {
+//     const [result] = await pool.query(`SELECT * FROM Livros WHERE ${parametro} = "${dados}"`)
+//     return result
+// }
 
-export async function getAutor(autor) {
-    const [result] = await pool.query("SELECT isbn FROM Autor WHERE autor = ?", [autor])
-    return result
-}
+// export async function getAutor(autor) {
+//     const [result] = await pool.query("SELECT isbn FROM Autor WHERE autor = ?", [autor])
+//     return result
+// }
 
-export async function getCategorialivro(categoria) {
-    const [result] = await pool.query("SELECT isbn FROM categoria_dos_livros WHERE categoria = ?", [categoria])
-    return result
-}
+// export async function getCategorialivro(categoria) {
+//     const [result] = await pool.query("SELECT isbn FROM categoria_dos_livros WHERE categoria = ?", [categoria])
+//     return result
+// }
 
-export async function getLivroByCategoria(categoria) {
-    const [result] = await pool.query(`
-    SELECT Livros.isbn,
-           Livros.titulo,
-           Livros.descricao,
-           Livros.data_de_aquisicao,
-           Livros.estado_de_conservacao,
-           Livros.localizacao_fisica,
-           Livros.uri_da_capa_do_livro 
-    FROM Livros, categoria_dos_livros 
-    WHERE Livros.isbn = categoria_dos_livros.isbn AND categoria_dos_livros.categoria = ?`, [categoria])
-    return result
-}
+// export async function getLivroByCategoria(categoria) {
+//     const [result] = await pool.query(`
+//     SELECT Livros.isbn,
+//            Livros.titulo,
+//            Livros.descricao,
+//            Livros.data_de_aquisicao,
+//            Livros.estado_de_conservacao,
+//            Livros.localizacao_fisica,
+//            Livros.uri_da_capa_do_livro 
+//     FROM Livros, categoria_dos_livros 
+//     WHERE Livros.isbn = categoria_dos_livros.isbn AND categoria_dos_livros.categoria = ?`, [categoria])
+//     return result
+// }
 
-export async function getLivroByAutor(autor) {
-    const [result] = await pool.query(`
-    SELECT Livros.isbn,
-           Livros.titulo,
-           Livros.descricao,
-           Livros.data_de_aquisicao,
-           Livros.estado_de_conservacao,
-           Livros.localizacao_fisica,
-           Livros.uri_da_capa_do_livro
-    FROM Livros, Autor 
-    WHERE Livros.isbn = Autor.isbn AND Autor.name = ?`, [autor])
-    return result
-}
+// export async function getLivroByAutor(autor) {
+//     const [result] = await pool.query(`
+//     SELECT Livros.isbn,
+//            Livros.titulo,
+//            Livros.descricao,
+//            Livros.data_de_aquisicao,
+//            Livros.estado_de_conservacao,
+//            Livros.localizacao_fisica,
+//            Livros.uri_da_capa_do_livro
+//     FROM Livros, Autor 
+//     WHERE Livros.isbn = Autor.isbn AND Autor.name = ?`, [autor])
+//     return result
+// }
 
 /* 
 --------------------------------
@@ -84,7 +84,7 @@ export async function getLivroByAutor(autor) {
 */
 
 // Função que cria livro
-export async function createLivro(isbn, titulo, descricao, data, estado, loc, uri) {
+export async function createLivro(isbn, titulo, descricao, data_de_aquisicao, estado_de_conservacao, localizacao_fisica, uri_da_capa_do_livro) {
     const result = await pool.query(`
     INSERT INTO Livros (isbn,
                         titulo,
@@ -94,7 +94,7 @@ export async function createLivro(isbn, titulo, descricao, data, estado, loc, ur
                         localizacao_fisica,
                         uri_da_capa_do_livro)
     VALUES (?, ?, ?, ?, ?, ?, ?)
-    `, [isbn, titulo, descricao, data, estado, loc, uri])
+    `, [isbn, titulo, descricao, data_de_aquisicao, estado_de_conservacao, localizacao_fisica, uri_da_capa_do_livro])
     return result
 }
 
@@ -104,7 +104,7 @@ export async function createLivro(isbn, titulo, descricao, data, estado, loc, ur
 --------------------------------
 */
 
-export async function editLivro(isbn, titulo, descricao, data, estado, loc, uri) {
+export async function editLivro(isbn, titulo, descricao, data_de_aquisicao, estado_de_conservacao, localizacao_fisica, uri_da_capa_do_livro) {
     const result = await pool.query(`
     UPDATE Livros
     SET 
@@ -117,7 +117,7 @@ export async function editLivro(isbn, titulo, descricao, data, estado, loc, uri)
         uri_da_capa_do_livro = ?
     WHERE
         isbn = ?;
-    `, [isbn, titulo, descricao, data, estado, loc, uri, isbn])
+    `, [isbn, titulo, descricao, data_de_aquisicao, estado_de_conservacao, localizacao_fisica, uri_da_capa_do_livro, isbn])
     return result
 }
 
@@ -154,16 +154,16 @@ export async function getMateriais() {
     return result
 }
 
-// Função que faz a query para buscar os materiais baseado no parametro
-export async function getBuscaMateriais(parametro, dados) {
-    const [result] = await pool.query(`SELECT * FROM Materiais_Didaticos WHERE ${parametro} = "${dados}"`)
-    return result
-}
+// // Função que faz a query para buscar os materiais baseado no parametro
+// export async function getBuscaMateriais(parametro, dados) {
+//     const [result] = await pool.query(`SELECT * FROM Materiais_Didaticos WHERE ${parametro} = "${dados}"`)
+//     return result
+// }
 
-export async function getCategoriaMaterial(categoria) {
-    const [result] = await pool.query("SELECT id FROM categoria_dos_materiais WHERE categoria = ?", [categoria])
-    return result
-}
+// export async function getCategoriaMaterial(categoria) {
+//     const [result] = await pool.query("SELECT id FROM categoria_dos_materiais WHERE categoria = ?", [categoria])
+//     return result
+// }
 
 /* 
 --------------------------------
@@ -171,7 +171,7 @@ export async function getCategoriaMaterial(categoria) {
 --------------------------------
 */
 
-export async function createMaterial(id, descricao, numero_de_serie, data, estado, loc, uri) {
+export async function createMaterial(id, descricao, numero_de_serie, data_de_aquisicao, estado_de_conservacao, localizacao_fisica, uri_da_foto_do_material) {
     const result = await pool.query(`
     INSERT INTO Materiais_Didaticos (
         id,
@@ -182,7 +182,7 @@ export async function createMaterial(id, descricao, numero_de_serie, data, estad
         localizacao_fisica,
         uri_da_foto_do_material)
     VALUES (?, ?, ?, ?, ?, ?, ?)
-    `, [id, descricao, numero_de_serie, data, estado, loc, uri])
+    `, [id, descricao, numero_de_serie, data_de_aquisicao, estado_de_conservacao, localizacao_fisica, uri_da_foto_do_material])
     return result
 }
 
@@ -192,7 +192,7 @@ export async function createMaterial(id, descricao, numero_de_serie, data, estad
 --------------------------------
 */
 
-export async function editMaterial(id, descricao, numero_de_serie, data, estado, loc, uri) {
+export async function editMaterial(id, descricao, numero_de_serie, data_de_aquisicao, estado_de_conservacao, localizacao_fisica, uri_da_foto_do_material) {
     const result = await pool.query(`
     UPDATE Materiais_Didaticos
     SET 
@@ -205,7 +205,7 @@ export async function editMaterial(id, descricao, numero_de_serie, data, estado,
         uri_da_foto_do_material = ?
     WHERE
         id = ?;
-    `, [id, descricao, numero_de_serie, data, estado, loc, uri, id])
+    `, [id, descricao, numero_de_serie, data_de_aquisicao, estado_de_conservacao, localizacao_fisica, uri_da_foto_do_material, id])
     return result
 }
 
@@ -241,10 +241,10 @@ export async function getUsers() {
     return result
 }
 
-export async function getUserByLogin(login) {
-    const [result] = await pool.query("SELECT * FROM Usuarios WHERE login = ?", [login])
-    return result
-}
+// export async function getUserByLogin(login) {
+//     const [result] = await pool.query("SELECT * FROM Usuarios WHERE login = ?", [login])
+//     return result
+// }
 
 /* 
 --------------------------------
@@ -252,7 +252,7 @@ export async function getUserByLogin(login) {
 --------------------------------
 */
 
-export async function createUser(id, nome, sobrenome, funcao, login, senha, uri) {
+export async function createUser(id, nome, sobrenome, funcao, login, senha, uri_da_foto_do_usuario) {
     const [result] = await pool.query("INSERT INTO Usuarios SET ?", {
     id: id,
     nome: nome,
@@ -260,7 +260,7 @@ export async function createUser(id, nome, sobrenome, funcao, login, senha, uri)
     funcao: funcao,
     login: login,
     senha: senha,
-    uri_da_foto_do_usuario: uri})
+    uri_da_foto_do_usuario: uri_da_foto_do_usuario})
     return result
 }
 
@@ -270,7 +270,7 @@ export async function createUser(id, nome, sobrenome, funcao, login, senha, uri)
 --------------------------------
 */
 
-export async function editUser(id, nome, sobrenome, funcao, login, senha, uri) {
+export async function editUser(id, nome, sobrenome, funcao, login, senha, uri_da_foto_do_usuario) {
     const result = await pool.query(`
     UPDATE Usuarios
     SET 
@@ -283,7 +283,7 @@ export async function editUser(id, nome, sobrenome, funcao, login, senha, uri) {
         uri_da_foto_do_usuario = ?
     WHERE
         id = ?;
-    `, [id, nome, sobrenome, funcao, login, senha, uri, id])
+    `, [id, nome, sobrenome, funcao, login, senha, uri_da_foto_do_usuario, id])
     return result
 }
 
@@ -308,59 +308,59 @@ export async function deleteUser(id) {
 ----------------------------------------------------------------
 */
 
-// Busca todos os emprestimos
-export async function getEmprestimo() {
-    const [result] = await pool.query("SELECT * FROM Emprestimos")
-    return result
-}
+// // Busca todos os emprestimos
+// export async function getEmprestimo() {
+//     const [result] = await pool.query("SELECT * FROM Emprestimos")
+//     return result
+// }
 
-// Cria emprestimos
-export async function createEmprestimo(id_do_livro, id_do_material, id_do_usuario,  data_do_emprestimo, data_de_devolucao_prevista, status_do_emprestimo) {
-    const result = await pool.query(`
-    INSERT INTO Emprestimos (id_do_livro,
-                        id_do_material,
-                        id_do_usuario,
-                        data_do_emprestimo,
-                        data_de_devolucao_prevista,
-                        status_do_emprestimo)
-    VALUES (?, ?, ?, ?, ?, ?)
-    `, [id_do_livro, id_do_material, id_do_usuario, data_do_emprestimo, data_de_devolucao_prevista, status_do_emprestimo])
-    return result
-}
+// // Cria emprestimos
+// export async function createEmprestimo(id_do_livro, id_do_material, id_do_usuario,  data_do_emprestimo, data_de_devolucao_prevista, status_do_emprestimo) {
+//     const result = await pool.query(`
+//     INSERT INTO Emprestimos (id_do_livro,
+//                         id_do_material,
+//                         id_do_usuario,
+//                         data_do_emprestimo,
+//                         data_de_devolucao_prevista,
+//                         status_do_emprestimo)
+//     VALUES (?, ?, ?, ?, ?, ?)
+//     `, [id_do_livro, id_do_material, id_do_usuario, data_do_emprestimo, data_de_devolucao_prevista, status_do_emprestimo])
+//     return result
+// }
 
-// atualiza data de emprestimo
-export async function atualizaEmprestimo(id_do_livro, date) {
-    const [result] = await pool.query(`UPDATE Emprestimos SET data_de_devolucao_prevista = "${date}" WHERE id_do_livro = "${id_do_livro}"` )
-    return result
-}
+// // atualiza data de emprestimo
+// export async function atualizaEmprestimo(id_do_livro, date) {
+//     const [result] = await pool.query(`UPDATE Emprestimos SET data_de_devolucao_prevista = "${date}" WHERE id_do_livro = "${id_do_livro}"` )
+//     return result
+// }
 
-export async function updateEmprestimoStatus(id, status, type) {
-    let id_type
-    switch (type) {
-        case "material":
-            id_type = "id_do_material"
-            break
-        case "livro":
-            id_type = "id_do_livro"
-            break
-        default:
-            return "Not a valid id type"
-    }
-    const [result] = await pool.query(`UPDATE Emprestimos SET status_do_emprestimo = "${status}" WHERE ${id_type} = "${id}"` )
-    return result
-}
+// export async function updateEmprestimoStatus(id, status, type) {
+//     let id_type
+//     switch (type) {
+//         case "material":
+//             id_type = "id_do_material"
+//             break
+//         case "livro":
+//             id_type = "id_do_livro"
+//             break
+//         default:
+//             return "Not a valid id type"
+//     }
+//     const [result] = await pool.query(`UPDATE Emprestimos SET status_do_emprestimo = "${status}" WHERE ${id_type} = "${id}"` )
+//     return result
+// }
 
-export async function getEmprestimoByParameter(parameter, value) {
-    const [result] = await pool.query(`SELECT * FROM Emprestimos WHERE ${parameter} = ?`, [value])
-    return result
-}
+// export async function getEmprestimoByParameter(parameter, value) {
+//     const [result] = await pool.query(`SELECT * FROM Emprestimos WHERE ${parameter} = ?`, [value])
+//     return result
+// }
 
-export async function getEmprestimoByMaterial(id) {
-    const [result] = await pool.query("SELECT * FROM Emprestimos WHERE id_do_material = ?", [id])
-    return result
-}
+// export async function getEmprestimoByMaterial(id) {
+//     const [result] = await pool.query("SELECT * FROM Emprestimos WHERE id_do_material = ?", [id])
+//     return result
+// }
 
-export async function getEmprestimoByUser(id) {
-    const [result] = await pool.query("SELECT * FROM Emprestimos WHERE id_do_usuario = ?", [id])
-    return result
-}
+// export async function getEmprestimoByUser(id) {
+//     const [result] = await pool.query("SELECT * FROM Emprestimos WHERE id_do_usuario = ?", [id])
+//     return result
+// }
