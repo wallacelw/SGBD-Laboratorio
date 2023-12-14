2ec6c61d7eb084c3b835d05689688d2b954ee0cb
Belphy <73123229+just-sleepy@users.noreply.github.com>
2023-12-14 13:49:07 -0300

    Update ListarEmprestimos.jsx

--- front/src/pages/ListarEmprestimos.jsx
+++ front/src/pages/ListarEmprestimos.jsx
@@ -11,31 +11,41 @@ function Emprestimos() {
   const [emprestimosFiltrados, setEmprestimosFiltrados] = useState([]);
   const navigate = useNavigate();
 
-  const [isAdmin, setIsAdmin] = useState(false)
+  const [isAdmin, setIsAdmin] = useState(false);
   useEffect(() => {
     const authLevel = localStorage.getItem("authLevel");
     setIsAdmin(authLevel == 2); // Setta admin como um booleano guardando o dado se o usuário atual é admin ou não
-  }, [isAdmin])
+  }, [isAdmin]);
 
   const handleEdit = async (emprestimo) => {
-    navigate(`/Edit-Emprestimo/${emprestimo.id}/${emprestimo.id_do_livro}/${emprestimo.id_do_material}/${emprestimo.id_do_usuario}`);
+    navigate(
+      `/Edit-Emprestimo/${emprestimo.id}/${emprestimo.id_do_livro}/${emprestimo.id_do_material}/${emprestimo.id_do_usuario}`
+    );
   };
 
   const handleReturn = async (emprestimo) => {
-    await axios.put("http://localhost:3333/status/", emprestimo, {headers: headers});
+    await axios.put("http://localhost:3333/status/", emprestimo, {
+      headers: headers,
+    });
     window.location.reload();
   };
 
   const handleDelete = async (emprestimo) => {
     try {
       if (emprestimo.id_do_livro) {
-        await axios.delete(`http://localhost:3333/emprestimo/byISBN/${emprestimo.id}/${emprestimo.id_do_livro}`, {headers: headers}).then(
-        (res) => toast(res.data.message))
-      }
-
-      else {
-        await axios.delete(`http://localhost:3333/emprestimo/byID/${emprestimo.id}/${emprestimo.id_do_material}`, {headers: headers}).then(
-        (res) => toast(res.data.message))
+        await axios
+          .delete(
+            `http://localhost:3333/emprestimo/byISBN/${emprestimo.id}/${emprestimo.id_do_livro}`,
+            { headers: headers }
+          )
+          .then((res) => toast(res.data.message));
+      } else {
+        await axios
+          .delete(
+            `http://localhost:3333/emprestimo/byID/${emprestimo.id}/${emprestimo.id_do_material}`,
+            { headers: headers }
+          )
+          .then((res) => toast(res.data.message));
       }
 
       window.location.reload();
@@ -57,8 +67,23 @@ function Emprestimos() {
             emprestimo.data_de_devolucao_prevista
           ).toLocaleDateString("pt-BR"),
         }));
-        setEmprestimos(emprestimosFormatados);
-        setEmprestimosFiltrados(emprestimosFormatados.filter((emprestimo) => emprestimo.id_do_usuario == localStorage.getItem("userId")));
+        if (localStorage.getItem("authLevel") < 2) {
+          setEmprestimos(
+            emprestimosFormatados.filter(
+              (emprestimo) =>
+                emprestimo.id_do_usuario == localStorage.getItem("userId")
+            )
+          );
+          setEmprestimosFiltrados(
+            emprestimosFormatados.filter(
+              (emprestimo) =>
+                emprestimo.id_do_usuario == localStorage.getItem("userId")
+            )
+          );
+        } else {
+          setEmprestimos(emprestimosFormatados);
+          setEmprestimosFiltrados(emprestimosFormatados);
+        }
       })
       .catch((error) => console.error("Erro ao buscar dados:", error));
   }, []);
@@ -121,26 +146,36 @@ function Emprestimos() {
               <td className="tabela">{emprestimo.id_do_material}</td>
               <td className="tabela">{emprestimo.id_do_usuario}</td>
               <td className="tabela">{emprestimo.data_do_emprestimo}</td>
-              <td className="tabela">{emprestimo.data_de_devolucao_prevista}</td>
+              <td className="tabela">
+                {emprestimo.data_de_devolucao_prevista}
+              </td>
               <td className="tabela">{emprestimo.status_do_emprestimo}</td>
               <td>
-                <button onClick={() => handleReturn(emprestimo)}>Devolver</button>
+                <button onClick={() => handleReturn(emprestimo)}>
+                  Devolver
+                </button>
               </td>
-              { isAdmin ?
-              <>
-              <td>
-                <button onClick={() => handleEdit(emprestimo)}>Editar</button>
-              </td>
-              <td>
-                <button onClick={() => handleDelete(emprestimo)}>Apagar</button>
-              </td>
-              </>
-              : null}
+              {isAdmin ? (
+                <>
+                  <td>
+                    <button onClick={() => handleEdit(emprestimo)}>
+                      Editar
+                    </button>
+                  </td>
+                  <td>
+                    <button onClick={() => handleDelete(emprestimo)}>
+                      Apagar
+                    </button>
+                  </td>
+                </>
+              ) : null}
             </tr>
           ))}
         </tbody>
       </table>
-      <button className="button_redirect" ><Link to="/"> Voltar para página inicial </Link></button>
+      <button className="button_redirect">
+        <Link to="/"> Voltar para página inicial </Link>
+      </button>
     </div>
   );
 }
